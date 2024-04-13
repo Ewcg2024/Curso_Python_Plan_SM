ef91fdd4adff4deae2184e26ef03a97533a13a3a
Ewcg2024 <166882003+Ewcg2024@users.noreply.github.com>
2024-04-13 18:54:30 -0500

    Agregado de Hola_Mundo.py
    Se agrego el archivo con las primeras pruebas de visual studio code

---
+++ Hola_Mundo.py
@@ -0,0 +1,9 @@
+#Función print permite imprimir y mostrarlos en el terminal
+print('hola a todos')
+print(10*5)
+"""
+Este es el primer programa que hago en python para desarrollo web
+Aqui podre hacer varias cosas con inteligencia artificial.   
+"""
+print(50-60)
+print('la suma de 1+10 es:', 1+10)
